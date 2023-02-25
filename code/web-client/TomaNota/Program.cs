4fd9d615b4ebf7e5bc2d562babea19b6ec22167b
milan-murray <milan-murray@outlook.ie>
2023-02-25 00:56:56 +0000

    Added user sign up

--- code/web-client/TomaNota/Program.cs
+++ code/web-client/TomaNota/Program.cs
@@ -1,11 +1,38 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Firebase.Auth;
+using Firebase.Auth.Providers;
+using Firebase.Auth.Repository;
 using TomaNota;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
+builder.Services.AddSingleton<IFirebaseAuthService, FirebaseAuthService>();
+
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 await builder.Build().RunAsync();
+
+/*
+// Configure...
+var config = new FirebaseAuthConfig
+{
+	ApiKey = "AIzaSyCLIUAHHeGxM5Pe_AdC8o4jGMIFwr3ZlH0",
+	AuthDomain = "tn-fb-ff018.firebaseapp.com",
+	Providers = new FirebaseAuthProvider[]
+	{
+        // Add and configure individual providers
+        new GoogleProvider().AddScopes("email"),
+		new EmailProvider()
+        // ...
+    },
+	// WPF:
+	UserRepository = new FileUserRepository("FirebaseSample") // persist data into %AppData%\FirebaseSample
+	// UWP:
+	// UserRepository = new StorageRepository() // persist data into ApplicationDataContainer
+};
+
+var client = new FirebaseAuthClient(config);
+*/
